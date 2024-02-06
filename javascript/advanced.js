632ba38f6fcfdaac9c4b6062f758971d62f71019
Spectrollay <2819288472@qq.com>
2024-02-06 11:58:04 +0800

    2024020602

--- javascript/advanced.js
+++ javascript/advanced.js
@@ -1,7 +1,9 @@
 function selectTab(tabNumber) {
     document.querySelectorAll('.tab_bar_btn').forEach(button => {
         button.classList.remove('active');
+        button.classList.add('no_active');
     });
 
     document.getElementById(`tab${tabNumber}`).classList.add('active');
+    document.getElementById(`tab${tabNumber}`).classList.remove('no_active');
 }
